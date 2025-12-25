33a9904c62932d9e48c8dc397b1f5ff4ea788931
takejohn <takejohn@takejohn.jp>
2025-12-25 19:54:07 +0900

    lintで自動修正

--- src/interpreter/index.ts
+++ src/interpreter/index.ts
@@ -3,21 +3,20 @@
  */
 
 import { autobind } from '../utils/mini-autobind.js';
-import { AiScriptError, NonAiScriptError, AiScriptNamespaceError, AiScriptIndexOutOfRangeError, AiScriptRuntimeError, AiScriptHostsideError } from '../error.js';
+import { AiScriptError, NonAiScriptError, AiScriptNamespaceError, AiScriptRuntimeError, AiScriptHostsideError } from '../error.js';
 import * as Ast from '../node.js';
 import { nodeToJs } from '../utils/node-to-js.js';
 import { Scope } from './scope.js';
 import { std } from './lib/std.js';
-import { RETURN, unWrapRet, BREAK, CONTINUE, assertValue, isControl, type Control, unWrapLabeledBreak } from './control.js';
-import { assertNumber, assertString, assertFunction, assertBoolean, assertObject, assertArray, eq, isObject, isArray, expectAny, reprValue, isFunction } from './util.js';
-import { NULL, FN_NATIVE, BOOL, NUM, STR, ARR, OBJ, FN, ERROR } from './value.js';
-import { getPrimProp } from './primitive-props.js';
+import { unWrapRet, assertValue, isControl, type Control } from './control.js';
+import { assertNumber, assertString, assertFunction, assertObject, assertArray, isObject, isArray, expectAny, reprValue, isFunction } from './util.js';
+import { NULL, FN_NATIVE, STR, ERROR } from './value.js';
 import { Variable } from './variable.js';
 import { Reference } from './reference.js';
-import type { JsValue } from './util.js';
-import type { Value, VFn, VUserFn } from './value.js';
-import type { AsyncEvaluationContext, CallInfo, LogObject, SyncEvaluationContext } from './evaluation.js';
 import * as evaluators from './evaluators.js';
+import type { JsValue } from './util.js';
+import type { Value, VFn } from './value.js';
+import type { AsyncEvaluationContext, CallInfo, LogObject, SyncEvaluationContext } from './evaluation.js';
 
 export class Interpreter {
 	public stepCount = 0;
